477206eebc355def5a5b97f24b8160c3dab18238
Matt Caswell <matt.caswell@hotmail.com>
2021-10-29 22:29:15 +0200

    fix mailgun integration

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
+require("dotenv").config();
 const express = require("express");
 const formidable = require("express-formidable");
 const cors = require("cors");
-require("dotenv").config();
-const mg = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
-
-const API_KEY = process.env.API_KEY;
-const DOMAIN = process.env.DOMAIN;
 
 const app = express();
 app.use(formidable());
 app.use(cors());
 
-app.get("/", (req, res)=>{
-  res.json("Welcome to my TripAdvisor-backend 🚀😎")
-})
+const API_KEY = process.env.API_KEY;
+const DOMAIN = process.env.DOMAIN;
+const mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
+
+app.get("/", (req, res) => {
+  res.json("Welcome to my TripAdvisor-backend 🚀😎");
+});
 
 app.post("/form", async (req, res) => {
-  try {
-    const data = {
-      from: `${req.fields.firstname} ${req.fields.lastname}  <${req.fields.email}>`,
-      to: "matt.caswell@hotmail.com",
-      subject: "Form submission!",
-      text: `${req.fields.message}`,
-    };
-    mg.messages().send(data, function (error, body) {
-      console.log(body);
-    });
-    res.status(200).json({ message: "Form successfully submitted" });
-  } catch (error) {
+  const data = {
+    from: `${req.fields.firstname} ${req.fields.lastname}  <${req.fields.email}>`,
+    to: "matt.caswell@hotmail.com",
+    subject: "Form submission!",
+    text: `${req.fields.message}`,
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    console.log(body);
     console.log(error);
-    res.status(400).json({ message: error.message });
-  }
+    if (error === undefined) {
+      // s'il n'y a pas eu d'erreur lors de l'envoi du mail, on envoie la réponse suivante au frontend :
+      res
+        .status(200)
+        .json({ message: "Données du form bien reçues, mail envoyé." });
+    } else {
+      // s'il y a eu une erreur lors de l'envoi du mail, on envoie la réponse suivante au frontend :
+      res.status(400).json(error);
+    }
+  });
 });
 
 app.listen(process.env.PORT, () => {
   console.log("Server started on Port 3000");
 });
+
+// "start": "node index.js"
