28563ccbbb6cbbd0285fe235fa2ac6461cae137e
Matt Caswell <matt.caswell@hotmail.com>
2021-10-29 18:07:10 +0200

    add mailgun settings

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const formidable = require("express-formidable");
 const cors = require("cors");
+require("dotenv").config();
+const mg = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
+
+const API_KEY = process.env.API_KEY;
+const DOMAIN = process.env.DOMAIN;
 
 const app = express();
 app.use(formidable());
@@ -8,7 +13,15 @@ app.use(cors());
 
 app.post("/form", async (req, res) => {
   try {
-    console.log(req.fields);
+    const data = {
+      from: `${req.fields.firstname} ${req.fields.lastname}  <${req.fields.email}>`,
+      to: "matt.caswell@hotmail.com",
+      subject: "Form submission!",
+      text: `${req.fields.message}`,
+    };
+    mg.messages().send(data, function (error, body) {
+      console.log(body);
+    });
     res.status(200).json({ message: "Form successfully submitted" });
   } catch (error) {
     console.log(error);
@@ -16,6 +29,6 @@ app.post("/form", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started on Port 3000");
 });
